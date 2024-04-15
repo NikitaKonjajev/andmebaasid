e7ed8f559823ba2fc558781bde5c4a8f8acfb904
NikitaKonjajev <nikita.kon00@gmail.com>
2024-04-15 14:05:56 +0300

    Tehtud kõik ilma kinokava

--- kinoteatr.sql
+++ kinoteatr.sql
@@ -38,3 +38,49 @@ SELECT * FROM zanr;
 INSERT INTO zanr( zanrnimi, zanrkirjeldus)
 VALUES('fentezi', 'Väha huvitav ja emotsionaalne film');
 SELECT * FROM zanr;
+
+
+--tabel piletiMyyk
+CREATE TABLE piletiMyyk(
+piletiMyykId int Primary Key identity(1,1),
+kogus int,
+kinokavaId int,
+);
+SELECT * FROM piletiMyyk;
+
+INSERT INTO piletiMyyk(kogus, kinokavaId)
+VALUES(3,3);
+SELECT * FROM piletiMyyk;
+
+
+--tabel kinokava
+CREATE TABLE kinokava(
+kinokavaId int Primary Key identity(1,1),
+kuupaev_kellaaeg datetime,
+filmnimetus int,
+pelitihind int,
+);
+ALTER TABLE kinokava DROP COLUMN kinokavaId
+SELECT * FROM kinokava;
+
+INSERT INTO kinokava(kuupaev_kellaaeg, filmNimetus, pelitihind)
+VALUES('15.04.24',  10, 12);
+SELECT * FROM kinokava;
+
+
+--tavel film
+CREATE TABLE film(
+filmId int Primary key identity(1,1),
+filmNimetus varchar(25),
+zanrId INT,
+pikkus INT,
+rezisorId int,
+filmTypeId int,
+reklaam image);
+select * from film;
+select * from filmType;
+--FK: film ->filmtype
+ALTER TABLE film add FOREIGN KEY(filmTypeId) references filmtype(filmTypeId);
+AlTER TABLE film add FOREIGN KEY(rezisorId) references rezisor(rezisorId);
+AlTER TABLE film add FOREIGN KEY(zanrId) references zanr(zanrId);
+AlTER TABLE piletiMyyk add FOREIGN KEY(kinokavaId) references kinokava(kinokavaId);
