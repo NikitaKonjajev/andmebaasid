1905cc3e8cfb81ae428f1c456e50511d764a81a3
NikitaKonjajev <nikita.kon00@gmail.com>
2024-04-15 13:06:15 +0300

    On valmis 3 tabelit ilma FK

--- kinoteatr.sql
+++ kinoteatr.sql
@@ -1,1 +1,40 @@
 CREATE DATABASE kinoteatrKonjajev;
+USE kinoteatrKonjajev;
+
+--tabel filmtype
+CREATE TABLE filmtype(
+filmTypeId int Primary Key identity(1,1),
+filmType varchar(25),
+kirjeldus TEXT,
+);
+SELECT * FROM filmtype;
+
+INSERT INTO filmtype( filmType, kirjeldus)
+VALUES('3D', 'väga huvitav fiml');
+SELECT * FROM filmtype;
+
+
+--tabel rezisor
+CREATE TABLE rezisor(
+rezisorId int Primary Key identity(1,1),
+eesnimi varchar(25),
+perenimi varchar(25),
+);
+SELECT * FROM rezisor;
+
+INSERT INTO rezisor( eesnimi, perenimi)
+VALUES('Max', 'DNS');
+SELECT * FROM rezisor;
+
+
+--tabel zanr
+CREATE TABLE zanr(
+zanrId int Primary Key identity(1,1),
+zanrnimi varchar(25),
+zanrkirjeldus TEXT,
+);
+SELECT * FROM zanr;
+
+INSERT INTO zanr( zanrnimi, zanrkirjeldus)
+VALUES('fentezi', 'Väha huvitav ja emotsionaalne film');
+SELECT * FROM zanr;
