23300fb380e70e8c3fed7005a15fc6df27b80a63
NikitaKonjajev <120027766+NikitaKonjajev@users.noreply.github.com>
2024-04-22 13:04:01 +0300

    tabeli film on loodud ja täidetud

--- proceduur.sql
+++ proceduur.sql
@@ -1,2 +1,64 @@
 create database KonjajevProcedure;
 use KonjajevProcedure;
+
+create table film(
+filmId int primary key identity(1,1),
+filmNimetus varchar(50),
+kestvus int,
+rezisoor varchar(50),
+v_aasta int)
+select * from film
+
+--mockaroo.com
+
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Kill Your Darlings', 180, 'Ben Goodenough', 1996);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Dead Man''s Walk', 156, 'Melisandra Poole', 2002);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Hail Columbia!', 251, 'Carol-jean Picken', 2011);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Daleks'' Invasion Earth: 2150 A.D.', 179, 'Aubrette Baudichon', 2008);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('R.S.V.P. ', 141, 'Sherri Yorkston', 2003);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Silverado', 106, 'Brier Pinor', 2007);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Percy Jackson: Sea of Monsters', 240, 'Wilow Vaskin', 1993);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Brother''s Kiss, A', 263, 'Kenon Brookes', 1998);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Wee Willie Winkie', 141, 'Allix Wattisham', 2002);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Generation Iron', 287, 'Shaylynn Porkiss', 2009);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Children of Invention', 268, 'Dev Lamborn', 1999);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Wrong Cops', 131, 'Emile Padrick', 2010);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Festival in Cannes', 134, 'Dag Barabisch', 2004);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Dominion: Prequel to the Exorcist', 235, 'Lombard Hruska', 2004);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Guys, The', 169, 'Sumner Aspling', 2001);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Last Winter, The', 231, 'Nadiya Marflitt', 1993);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Gentleman Jim', 199, 'Alistair Dutson', 1997);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Endless Summer, The', 195, 'Eugenie Blundell', 1999);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Mr. Nice', 290, 'Brittaney Brundall', 2002);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Hester Street', 162, 'Neron Aitkin', 1998);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Insider, The', 298, 'Stephana Pechell', 2001);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Dunwich Horror, The', 232, 'Sawyer Simkin', 2000);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Ten, The', 208, 'Steven Stamps', 2006);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Condo Painting', 151, 'Brittne Lawlee', 1992);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Troy', 248, 'Raf Swindall', 2010);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Hope Springs', 111, 'Cordelie Howatt', 2006);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Double Trouble', 172, 'Delphine Vigurs', 1993);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Bye-Bye', 130, 'Mable Hagard', 1979);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Growing Pains', 235, 'Mariana Wilding', 2004);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Die Frau des Frisörs', 217, 'Idaline Boys', 2007);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('The Wedding Ringer', 162, 'Ozzy Chastey', 2001);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Secret Ceremony', 247, 'Petra Poor', 2012);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Secret Life of Walter Mitty, The', 172, 'Joe Hullett', 1995);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Baffled!', 261, 'Dell Imorts', 2006);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Terror Train', 236, 'Syd Breckin', 2008);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('See Heaven', 119, 'Marinna Sylvaine', 1996);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Waiting...', 243, 'Merissa Howitt', 2005);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Isn''t She Great?', 236, 'Arturo Hamlen', 2000);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Andy Hardy''s Double Life', 156, 'Bambie Maestrini', 2008);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Thirteenth Floor, The', 108, 'Brandais Juppe', 1988);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('George Lopez: Tall, Dark & Chicano', 292, 'Bald Bull', 1994);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Babysitter Wanted', 180, 'Cherilynn Morrell', 2012);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Man on a Ledge', 170, 'Martin Tilston', 2009);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Son of Dracula', 159, 'Ellerey Joris', 1999);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Recoil', 150, 'Currie Romanet', 2011);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Russell Peters: Outsourced', 117, 'Friederike Andreix', 1984);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Lifeforce', 210, 'Alma Ivchenko', 2006);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('NeverEnding Story, The', 193, 'Elvira Stickney', 2005);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Dog Day (Canicule)', 248, 'Rhianon Harcarse', 1991);
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values ('Mr. Nice Guy (Yat goh ho yan)', 221, 'Bobine Dumphy', 1990);
+select * from film
