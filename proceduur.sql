4e03ae8fc3f84cf648b09455ff722af69a32b9fa
NikitaKonjajev <120027766+NikitaKonjajev@users.noreply.github.com>
2024-04-24 12:18:18 +0300

    proceduuri käivitamine

--- proceduur.sql
+++ proceduur.sql
@@ -5,3 +5,12 @@ kestvus int,
 rezisoor varchar(50),
 v_aasta int)
 
+create proceduur 'otsin1taht'(In 'taht' char(1)) not deterministic contains sql sql security definder
+  
+begin
+	select * from film
+	where filmNimetus like CONCAT(@taht,'%');
+End;
+
+--protseduuri käivitamine
+call otsin1taht ('C'); 
